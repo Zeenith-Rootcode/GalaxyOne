52375e22a09b2978d8ba9be311442892b086efbe
MiuruHan <abeysinghechanuka@gmail.com>
2023-08-18 10:52:48 +0530

    Resolved exeisting password in google path

--- Backend/api/controllers/userController.js
+++ Backend/api/controllers/userController.js
@@ -62,6 +62,33 @@ exports.registerUser = async (req, res) => {
                 race,
                 password,
               });
+
+              bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if (err) throw err;
+                  newUser.password = hash;
+                  newUser
+                    .save()
+                    .then((responseSavingUser) => {
+                      console.log(responseSavingUser);
+                      res.json({
+                        Error: false,
+                        Status: 200,
+                        Message: "Registration succeeded.",
+                        User: responseSavingUser,
+                      });
+                    })
+                    .catch((error) => {
+                      console.log(error);
+                      res.json({
+                        Error: true,
+                        Status: 400,
+                        Message:
+                          "Registration was unsuccessful : " + error.message,
+                      });
+                    });
+                });
+              });
             } else if (loginType === "GOOGLE") {
               if (googleId === "" || !googleId) {
                 res.json({
@@ -79,8 +106,27 @@ exports.registerUser = async (req, res) => {
                 homePlanet,
                 homeDestrict,
                 race,
-                password,
               });
+
+              newUser
+                .save()
+                .then((responseSavingUser) => {
+                  console.log(responseSavingUser);
+                  res.json({
+                    Error: false,
+                    Status: 200,
+                    Message: "Registration succeeded.",
+                    User: responseSavingUser,
+                  });
+                })
+                .catch((error) => {
+                  console.log(error);
+                  res.json({
+                    Error: true,
+                    Status: 400,
+                    Message: "Registration was unsuccessful : " + error.message,
+                  });
+                });
             } else {
               res.json({
                 Error: true,
@@ -88,32 +134,6 @@ exports.registerUser = async (req, res) => {
                 Message: "Invalid login type.",
               });
             }
-            bcrypt.genSalt(10, (err, salt) => {
-              bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser
-                  .save()
-                  .then((responseSavingUser) => {
-                    console.log(responseSavingUser);
-                    res.json({
-                      Error: false,
-                      Status: 200,
-                      Message: "Registration succeeded.",
-                      User: responseSavingUser,
-                    });
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                    res.json({
-                      Error: true,
-                      Status: 400,
-                      Message:
-                        "Registration was unsuccessful : " + error.message,
-                    });
-                  });
-              });
-            });
           }
         })
         .catch((error) => {
@@ -236,153 +256,4 @@ exports.getUsers = async (req, res) => {
         Error: error,
       });
     });
-};
-
-exports.getUserRecords = (req, res) => {
-  const { userId } = req.body;
-
-  if (userId === null || userId === "") {
-    res.json({
-      Status: "Unsuccessful",
-      Message: "Email and password must be entered.",
-    });
-  } else {
-    User.findById(userId)
-      .then((user) => {
-        if (user) {
-          UserFatLevelRecords.find({ userId: userId })
-            .then((userFatLevelRecords) => {
-              UserMoodRecords.find({ userId: userId })
-                .then((userMoodRecords) => {
-                  res.json({
-                    Status: "Successful",
-                    UserFatLevels: userFatLevelRecords,
-                    UserMoods: userMoodRecords,
-                  });
-                })
-                .catch((error) => {
-                  console.log(error);
-                  res.json({
-                    Status: "Unsuccessful",
-                    Error: error,
-                    Message: "Happened when getting fat levels records.",
-                  });
-                });
-            })
-            .catch((error) => {
-              console.log(error);
-              res.json({
-                Status: "Unsuccessful",
-                Error: error,
-                Message: "Happened when getting fat levels records.",
-              });
-            });
-        } else {
-          console.log("BAAA");
-
-          res.json({ Status: "Unsuccessful", Message: "Invalid user id." });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        res.json({
-          Status: "Unsuccessful",
-          Error: error,
-        });
-      });
-  }
-};
-
-exports.addAppointment = (req, res) => {
-  const {
-    userId,
-    appointmentDate,
-    appointmentTime,
-    age,
-    scholarship,
-    hypertension,
-    diabetes,
-    alcoholism,
-    handcap,
-    sms,
-    gender,
-  } = req.body;
-
-  if (
-    userId === "" ||
-    age === null ||
-    scholarship === null ||
-    hypertension === null ||
-    diabetes === null ||
-    alcoholism === null ||
-    handcap === null ||
-    sms === null ||
-    gender === null ||
-    appointmentDate === null ||
-    appointmentTime === null
-  ) {
-    res.json({ Status: "Unsuccessful", Message: "All data must be entered." });
-  } else {
-    const options = {
-      method: "POST",
-      uri: PYTHON_FLASK_API_URL + "predictNoShow",
-      body: {
-        modelInput: [
-          [
-            parseFloat(age),
-            parseInt(scholarship),
-            parseInt(hypertension),
-            parseInt(diabetes),
-            parseInt(alcoholism),
-            parseInt(handcap),
-            parseInt(sms),
-            parseInt(gender),
-          ],
-        ],
-      },
-      json: true,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-
-    request(options).then(function (response) {
-      let dataFromFlaskAPI = JSON.stringify(response);
-      dataFromFlaskAPI = JSON.parse(dataFromFlaskAPI);
-      dataFromFlaskAPI = dataFromFlaskAPI.prediction;
-      let pred1 = dataFromFlaskAPI[0][0];
-      let pred2 = dataFromFlaskAPI[0][1];
-      let noShow;
-      if (pred1 >= pred2) {
-        noShow = true;
-      } else {
-        noShow = false;
-      }
-      const newRecord = new Appointments({
-        userId,
-        age,
-        noShow,
-        appointmentDate,
-        appointmentTime,
-      });
-      newRecord
-        .save()
-        .then((result) => {
-          console.log(result);
-          res.json({
-            Status: "Successful",
-            Message: "Record  has been saved successfully.",
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-
-          res.json({
-            Status: "Unsuccessful",
-            Message: "Happened while saving the body fat record in the DB.",
-            error: error,
-          });
-        });
-    });
-  }
 };
