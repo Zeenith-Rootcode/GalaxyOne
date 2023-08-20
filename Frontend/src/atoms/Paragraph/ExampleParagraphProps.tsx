b4bb349e37e09cbf67535caf3512cc7d7797d91f
Nilupa Illangarathna <nilupalakshantha50@gmail.com>
2023-08-20 21:28:21 +0530

    Welcomepage and Homepage added

--- Frontend/src/atoms/Paragraph/ExampleParagraphProps.tsx
+++ Frontend/src/atoms/Paragraph/ExampleParagraphProps.tsx
@@ -2,38 +2,26 @@ import React from 'react';
 import Paragraph from './Paragraph'; // Update the path as needed
 import { ParagraphProps } from './ParagraphProps';
 
-export const exampleParagraphProps1: ParagraphProps = {
-  id: 'intro-paragraph',
-  className: 'intro-paragraph',
-  style: { color: '#333' },
-  lang: 'en',
-  dir: 'ltr',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada in lectus non consequat.',
-};
+// export const exampleParagraphProps1: ParagraphProps = {
+//   id: 'intro-paragraph',
+//   className: 'intro-paragraph',
+//   style: { color: '#333' },
+//   lang: 'en',
+//   dir: 'ltr',
+//   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada in lectus non consequat.',
+// };
 
-export const exampleParagraphProps2: ParagraphProps = {
-    id: 'highlighted-paragraph',
-    className: 'highlighted-paragraph',
-    style: { backgroundColor: 'yellow', padding: '10px' },
-    lang: 'en',
-    dir: 'ltr',
-    text: 'This paragraph is highlighted with a yellow background.',
-  };
+type fontSize = 10 |20| 25;
 
-  export const exampleParagraphProps3: ParagraphProps = {
-    id: 'rtl-paragraph',
-    className: 'rtl-paragraph',
-    style: { border: '1px solid red', padding: '5px' },
-    lang: 'en',
-    dir: 'rtl',
-    text: 'Suspendisse malesuada in lectus non consequat.',
-  };
-
-  export const exampleParagraphProps4: ParagraphProps = {
-    id: 'large-font-paragraph',
-    className: 'large-font-paragraph',
-    style: { fontSize: '24px', lineHeight: '1.5', fontFamily: 'Arial, sans-serif' },
-    lang: 'en',
-    dir: 'ltr',
-    text: 'This paragraph has a larger font size and increased line height.',
-  };
+// HomePage related
+export function textSize(fontSize: fontSize, text: string){
+    const Your_Portal_to_the_Cosmos: ParagraphProps = {
+      id: 'intro-paragraph',
+      className: 'intro-paragraph',
+      style: { color: 'white', fontSize: fontSize, fontWeight:'bold'},
+      lang: 'en',
+      dir: 'ltr',
+      text: text,
+    };
+    return Your_Portal_to_the_Cosmos;
+}
---
+++ Frontend/src/organisms/HomePage/HomePage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from '../../atoms/Image/Image';
+import {imageProvider} from '../../atoms/Image/ExampleImageProps'; // Update the path as needed
+import Paragraph from '../../atoms/Paragraph/Paragraph';
+import {textSize} from '../../atoms/Paragraph/ExampleParagraphProps';
+import Button from '../../atoms/Button/Button';
+import { exampleButtonProps } from '../../atoms/Button/ExampleButtonProps';
+
+const centerStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    padding: '0px',
+    backgroundColor:  'rgba(0, 0, 0, 0.0)', // Transparent black background
+  };
+
+  const HomePage: React.FC = () => {
+    return (
+      <div style={centerStyle}>
+        <Image {...imageProvider('https://picsum.photos/2000/2000?random=1')} />
+        <Paragraph {...textSize(20, "GalaxyOne")}/>
+        <Paragraph {...textSize(10, "Your Portal to the Cosmos")}/>
+        <Button {...exampleButtonProps} text="Go Now" />
+        <Button {...exampleButtonProps} text="Explore"  />
+        <Button {...exampleButtonProps} text="History" />
+        <Button {...exampleButtonProps} text="About Us" />
+        
+      </div>
+    );
+  };
+  
+  export default HomePage;
